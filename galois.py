d62ca243196359c7ef83f55f377c93e9608731ef
David Ivan <avandavad@gmail.com>
2019-06-22 10:25:24 +0200

    - refactoring Scalar - adding rand() classmethod

--- galois.py
+++ galois.py
@@ -3,39 +3,25 @@ from utils import (bitlist_from_hex,
                    hex_from_bitlist, 
                    exponents_from_bitlist, 
                    bitlist_from_exponents)
-
+import random
 
 
 class Scalar:
     '''
-    element of Galois Field (256)
+    element of GaloisField(256)
     this is actually a byte
     '''
     def __init__(self, h_str):
         '''
         e.g.: Scalar('a8')
         '''
+        self.h_str = h_str
         assert len(h_str) == 2
         for c in h_str:
             assert c in '0123456789abcdef'
         self.bitlist = bitlist_from_hex(h_str)
         exponents = exponents_from_bitlist(self.bitlist)
         self._poly = Polynom2(exponents)
-        
-    def __add__(self, other):
-        new_poly = self._poly + other._poly
-        return Scalar.from_polynom2(new_poly)
-    
-    def __mul__(self, other):
-        new_poly = (self._poly * other._poly) % AES_P
-        return Scalar.from_polynom2(new_poly)
-    
-    def __repr__(self):
-        return hex_from_bitlist(self.bitlist)
-    
-    def inverse(self):
-        p_inv = inverse(self._poly, AES_P)[0]
-        return Scalar.from_polynom2(p_inv)
     
     @classmethod
     def from_bitlist(cls, bitlist):
@@ -47,5 +33,29 @@ class Scalar:
         exponents = poly.exponents
         bitlist = bitlist_from_exponents(exponents)
         return cls.from_bitlist(bitlist)
+    
+    @classmethod
+    def rand(cls):
+        bitlist = []
+        for i in range(8):
+            bitlist.append(random.randint(0,1))
+        return cls.from_bitlist(bitlist)
+    
+    def __add__(self, other):
+        assert isinstance(other, Scalar)
+        new_poly = self._poly + other._poly
+        return Scalar.from_polynom2(new_poly)
+    
+    def __mul__(self, other):
+        assert isinstance(other, Scalar)
+        new_poly = (self._poly * other._poly) % AES_P
+        return Scalar.from_polynom2(new_poly)
+    
+    def __repr__(self):
+        return self.h_str
+    
+    def inverse(self):
+        p_inv = inverse(self._poly, AES_P)[0]
+        return Scalar.from_polynom2(p_inv)
     
 
