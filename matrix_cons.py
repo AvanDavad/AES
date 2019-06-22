7ba2143c05b0392de654a5398a9a5fdff0cbbf3d
David Ivan <avandavad@gmail.com>
2019-06-22 16:45:57 +0200

    calculating the inverse of COL_MIX rather than hard coding

--- matrix_cons.py
+++ matrix_cons.py
@@ -5,9 +5,5 @@ COL_MIX = Mat4(['02 03 01 01',
                 '01 01 02 03', 
                 '03 01 01 02'])
 
-COL_MIX_INV = Mat4(['0e 0b 0d 09', 
-                    '09 0e 0b 0d', 
-                    '0d 09 0e 0b', 
-                    '0b 0d 09 0e'])
-
+COL_MIX_INV = COL_MIX.inverse()
 assert COL_MIX @ COL_MIX_INV == Mat4.eye()
